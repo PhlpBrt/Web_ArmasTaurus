33c739452f1e9d84b474e33b601fae632f7dfe06
Philipe Brito <38106689+PhlpBrt@users.noreply.github.com>
2020-05-04 22:43:59 -0300

    Finaly

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
 function onOff() {
     document.querySelector("#modal").classList.toggle("hide")
 }
+var btn = document.querySelector('button');
+btn.onclick = function mostra(id) {
+    if (document.getElementById(id).style.display == 'none') {
+
+
+        document.getElementById(id).style.display = 'grid';
+    }
+    else {
+        document.getElementById(id).style.display == 'none'
+    }
+}
+
 
